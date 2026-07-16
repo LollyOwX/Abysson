dc9104649e53a5411757d9cae571d0d86fd369a1
LollyOwX <lollyowx@gmail.com>
2026-07-16 11:01:18 +0200

    0.9.1 Items

---
+++ src/items/Shield_Basic_Wood.java
@@ -0,0 +1,13 @@
+package items;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class Shield_Basic_Wood extends Entity {
+    public Shield_Basic_Wood(GamePanel gp) {
+        super(gp);
+
+        name = "Basic Wood Shield";
+
+    }
+}
---
+++ src/items/Sword_Basic_Iron.java
@@ -0,0 +1,12 @@
+package items;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class Sword_Basic_Iron extends Entity {
+    public Sword_Basic_Iron(GamePanel gp) {
+        super(gp);
+
+        name = "Basic Iron Sword";
+    }
+}
